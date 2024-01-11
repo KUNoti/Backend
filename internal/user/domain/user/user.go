f669210305022343dcb169ec6e9518fc161306b0
WhatWR <kul.b@ku.th>
2024-01-11 13:05:39 +0700

    encode login username & password

--- internal/user/domain/user/user.go
+++ internal/user/domain/user/user.go
@@ -2,12 +2,17 @@ package user
 
 import (
 	"bytes"
+	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +21,7 @@ import (
 const (
 	LOGINLINK       = "https://myapi.ku.th/auth/login"
 	GETSCHEDULELINK = "https://myapi.ku.th/std-profile/getGroupCourse"
+	KUPUBLICKEY     = "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAytOhlq/JPcTN0fX+VqObE5kwIaDnEtso2KGHdi9y7uTtQA6pO4fsPNJqtXOdrcfDgp/EQifPwVRZpjdbVrD6FgayrQQILAnARKzVmzwSMDdaP/hOB6i9ouKsIhN9hQUmUhbhaMkh7UXoxGW+gCSK8dq0+FJVnlt1dtJByiVAJRi2oKSdLRqNjk8yGzuZ6SrEFzAgYZwmQiywUF6V1ZaMUQDz8+nr9OOVU3c6Z2IQXCbOv6S7TAg0VhriFL18ZxUPS6759SuKC63VOOSf4EEHy1m0qBgpCzzlsB7D4ssF9x0ZVXLREFrqikP71Hg6tSGcu4YBKL+VwIDWWaXzz6szxeDXdYTA3l35P7I9uBUgMznIjTjNaAX4AXRsJcN9fpF7mVq4eK1CorBY+OOzOc+/yVBpKysdaV/yZ+ABEhX93B2kPLFSOPUKjSPK2rtqE6h2NSl5BFuGEoVBerKn+ymOnmE4/SDBSe5S6gIL5vwy5zNMsxWUaUF5XO9Ez+2v8+yPSvQydj3pw5Rlb07mAXcI18ZYGClO6g/aKL52KYnn1FZ/X3r8r/cibfDbuXC6FRfVXJmzikVUqZdTp0tOwPkh4V0R63l2RO9Luy7vG6rurANSFnUA9n842KkRtBagQeQC96dbC0ebhTj+NPmskklxr6/6Op/P7d+YY76WzvQMvnsCAwEAAQ==\n-----END PUBLIC KEY-----"
 )
 
 type Test struct {
@@ -45,63 +51,87 @@ func errorResponse(err error) gin.H {
 }
 
 func LoginUser(c *gin.Context) {
-	var reqBody map[string]string
+	var reqBody LoginUserRequest
 	if err := c.BindJSON(&reqBody); err != nil {
 		c.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
-	encodedBody := map[string]string{
-		"username": encodeString(reqBody["username"]),
-		"password": encodeString(reqBody["password"]),
+	username, err := encodeString(reqBody.Username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
 	}
 
-	// headers := map[string]string{
-	// 	"app-key": "apikey",
-	// }
+	password, err := encodeString(reqBody.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	encodedBody := map[string]string{
+		"username": username,
+		"password": password,
+	}
 
 	encodedJSON, err := json.Marshal(encodedBody)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
 	}
 
-	resp, err := http.Post(LOGINLINK, "application/json", bytes.NewBuffer(encodedJSON))
+	fmt.Println("Encode JSON: ", encodedBody)
 
+	req, err := http.NewRequest(http.MethodPost, LOGINLINK, bytes.NewBuffer(encodedJSON))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
+
+	req.Header.Set("app-key", "txCR5732xYYWDGdd49M3R19o1OVwdRFc")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		// panic(err)
+		fmt.Println("Something wrong: ", err)
+	}
+
 	defer resp.Body.Close()
 
-	// Handle response logic here
+	respBody, _ := io.ReadAll(resp.Body)
+	fmt.Printf("Respopnse Body: %s", respBody)
 
-	c.JSON(resp.StatusCode, gin.H{"data": "response data"}) // replace with actual response data
+	c.JSON(resp.StatusCode, respBody)
 }
 
-func encodeString(data string) string {
-	block, _ := pem.Decode([]byte("kuPublicKey"))
+func encodeString(data string) (string, error) {
+	keyPEM := "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAytOhlq/JPcTN0fX+VqObE5kwIaDnEtso2KGHdi9y7uTtQA6pO4fsPNJqtXOdrcfDgp/EQifPwVRZpjdbVrD6FgayrQQILAnARKzVmzwSMDdaP/hOB6i9ouKsIhN9hQUmUhbhaMkh7UXoxGW+gCSK8dq0+FJVnlt1dtJByiVAJRi2oKSdLRqNjk8yGzuZ6SrEFzAgYZwmQiywUF6V1ZaMUQDz8+nr9OOVU3c6Z2IQXCbOv6S7TAg0VhriFL18ZxUPS6759SuKC63VOOSf4EEHy1m0qBgpCzzlsB7D4ssF9x0ZVXLREFrqikP71Hg6tSGcu4YBKL+VwIDWWaXzz6szxeDXdYTA3l35P7I9uBUgMznIjTjNaAX4AXRsJcN9fpF7mVq4eK1CorBY+OOzOc+/yVBpKysdaV/yZ+ABEhX93B2kPLFSOPUKjSPK2rtqE6h2NSl5BFuGEoVBerKn+ymOnmE4/SDBSe5S6gIL5vwy5zNMsxWUaUF5XO9Ez+2v8+yPSvQydj3pw5Rlb07mAXcI18ZYGClO6g/aKL52KYnn1FZ/X3r8r/cibfDbuXC6FRfVXJmzikVUqZdTp0tOwPkh4V0R63l2RO9Luy7vG6rurANSFnUA9n842KkRtBagQeQC96dbC0ebhTj+NPmskklxr6/6Op/P7d+YY76WzvQMvnsCAwEAAQ==\n-----END PUBLIC KEY-----"
+	publicKey := strings.ReplaceAll(keyPEM, "\\n", "\n")
+
+	block, _ := pem.Decode([]byte(publicKey))
 	if block == nil {
-		panic("Failed to decode PEM block containing public key")
+		return "", fmt.Errorf("failed to parse PEM block containing the public key")
 	}
 
-	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+
 	if err != nil {
-		panic("Failed to parse public key")
+		return "", nil
 	}
 
-	rsaPubKey, ok := pubKey.(*rsa.PublicKey)
+	rsaPub, ok := pub.(*rsa.PublicKey)
 	if !ok {
-		panic("Failed to convert public key to RSA public key")
+		return "", fmt.Errorf("failed to convert public key to RSA public key")
 	}
 
-	encryptedData, err := rsa.EncryptPKCS1v15(nil, rsaPubKey, []byte(data))
+	ciphertext, err := rsa.EncryptOAEP(
+		sha256.New(),
+		rand.Reader,
+		rsaPub,
+		[]byte(data),
+		nil,
+	)
 	if err != nil {
-		panic("Failed to encrypt data")
+		return "", err
 	}
 
-	return base64.StdEncoding.EncodeToString(encryptedData)
-}
-
-func getSchedule(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"test": "test"})
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
